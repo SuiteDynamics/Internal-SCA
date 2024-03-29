9aa43b1ce90809384ee2ef11d740ed84e12a3ab2
Eric Laylan <elaylan@anchorgroup.tech>
2024-03-29 14:08:16 -0500

    motus clean up

--- compare.js
+++ compare.js
@@ -36,6 +36,7 @@
                             new nlobjSearchColumn('custrecord_motus_cc_token_exp_month'),
                             new nlobjSearchColumn('custrecord_motus_cc_token_exp_year'),
                             new nlobjSearchColumn('custrecord_motus_cc_token_token'),
+                            new nlobjSearchColumn('custrecord_motus_cus_prof_id'),
                             new nlobjSearchColumn('internalid')
                         ];
                         var search_results = Application.getAllSearchResults('customrecord_motus_cc_token', filters, columns);
@@ -51,7 +52,7 @@
                                 motus_card.exp_month = card.getValue('custrecord_motus_cc_token_exp_month');
                                 motus_card.exp_year = card.getValue('custrecord_motus_cc_token_exp_year');
                                 motus_card.token = card.getValue('custrecord_motus_cc_token_token');
-                                //motus_card.csc = card.getValue('custrecord_motus_cc_csc');
+                                motus_card.uuid = card.getValue('custrecord_motus_cus_prof_id');
                                 motus_card.id = card.getValue('internalid');
                                 motus_card.active = false;
                         
@@ -185,6 +186,7 @@
                         nlapiLogExecution('DEBUG', 'PROFILE_TOKENDataCSC', JSON.stringify(data));
                         nlapiLogExecution('DEBUG', 'PROFILE_OAUTHTOKENData', JSON.stringify(oauthToken));
 
+                        //Use filler address because the customer has not submitted their address in checkout yet
                         var motusBody = {
                             "customer_id":data.uuid,
                             "hpf_token":data.hpf_token,
@@ -192,6 +194,15 @@
                             "integrator_id": motusCredentials.integrationid,
                             "phone"		: data.phone,
                             "email"		: data.email,
+                            "billing_address":{
+                                "name": data.billingAddr.name,
+                                "street_address": data.billingAddr.street_address,
+                                "street_address2": data.billingAddr.street_address2,
+                                "city": data.billingAddr.city,
+                                "state": data.billingAddr.state,
+                                "zip": data.billingAddr.zip,
+                                "country": data.billingAddr.country,
+                            }
                         }
 
                         //Build header object
@@ -228,6 +239,71 @@
                             
                         }else{
                             nlapiLogExecution('ERROR', 'CUST_PROFILE_ERR', JSON.stringify(responseBody));
+                            return{
+                                status: 'Error',
+                                response: responseBody
+                            }
+                        }
+                    },
+                    submitBillingAddress: function(motusCredentials, oauthToken, data){
+                        // nlapiLogExecution('DEBUG', 'submit', JSON.stringify(data));
+                        nlapiLogExecution('DEBUG', 'submitBillingAddressdata', JSON.stringify(data));
+                        nlapiLogExecution('DEBUG', 'submitBillingAddressoauth', JSON.stringify(oauthToken));
+
+                        hpf_enc = nlapiLookupField('customrecord_motus_cc_token', data.id,['custrecord_motus_cc_token_enc','custrecord_motus_cc_token_hpf'])
+                        
+                        //Use filler address because the customer has not submitted their address in checkout yet
+                        var motusBody = {
+                            "customer_id": data.uuid,
+                            "hpf_token":hpf_enc.custrecord_motus_cc_token_hpf,
+                            "enc_key":hpf_enc.custrecord_motus_cc_token_enc,
+                            "integrator_id": motusCredentials.integrationid,
+                            "phone"		: data.phone,
+                            "email"		: data.email,
+                            "billing_address":{
+                                "name": data.data.billingAddrObj.name,
+                                "street_address":data.data.billingAddrObj.street_address,
+                                //"street_address2":data.data.billingAddrObj.street_address2,
+                                "city":data.data.billingAddrObj.city,
+                                "state":data.data.billingAddrObj.state,
+                                "zip":data.data.billingAddrObj.zip
+                            }
+                        }
+                        nlapiLogExecution('DEBUG', 'submitBillingAddressbody', JSON.stringify(motusBody))
+                        //Build header object
+                        var headerObj = {
+                            'Authorization': oauthToken.token_type + " " + oauthToken.access_token,
+                            "Access-Control-Allow-Origin": "*",
+                            "Accept": "application/json",
+                            "Accept-Encoding": "gzip, deflate, br",
+                            "Content-Type": "application/json"
+                        }
+                        var motusUpdateCustApiUrl = motusCredentials.baseurl + '/v1/customer/pt_protect_update'
+                        nlapiLogExecution('DEBUG', 'body', JSON.stringify(motusBody));
+                        nlapiLogExecution('DEBUG', 'motusPaymentsApiUrl', motusUpdateCustApiUrl);
+                        nlapiLogExecution('DEBUG', 'headers', JSON.stringify(headerObj));
+                        // var response = nlapiRequestURL(motusUpdateCustApiUrl, body, headerObj)
+                        nlapiLogExecution('ERROR', 'PROFILE submit', JSON.stringify(motusBody));
+                        var response = nlapiRequestURL(motusUpdateCustApiUrl, JSON.stringify(motusBody), headerObj, 'POST')
+                        // var response = nlapiRequestURL(url, body, header, null, "POST");
+                        nlapiLogExecution('DEBUG', 'RAW_OAUTH_RESPONSE', JSON.stringify(response));
+                        var responseBody = JSON.parse(response.getBody());
+                        nlapiLogExecution('ERROR', 'PROFILE_RESPONSE', JSON.stringify(responseBody));
+                        
+                        if (response.getCode() == 200 && responseBody.success == true) {
+                        
+                            //if new profile, submit id to customer
+                            // if(userMotusId == ""){
+                            //     nlapiSubmitField('customer', user, 'custentity_profile_id_motus', responseBody.profileid);
+                            // }
+                            
+                            return{
+                                status: 'Success',
+                                response: responseBody
+                            }
+                            
+                        }else{
+                            nlapiLogExecution('ERROR', 'UPDATE_CUST_PROFILE_ERR', JSON.stringify(responseBody));
                             return{
                                 status: 'Error',
                                 response: responseBody
@@ -293,7 +369,9 @@
                         motusTokenRecord.setFieldValue('custrecord_motus_cc_token_exp_year', cardDetails.response.customers[0].credit_card.expiration_year.toString());
                         motusTokenRecord.setFieldValue('custrecord_sd_motus_cc_last_4', cardDetails.response.customers[0].credit_card.masked_number.slice(-4));
                         motusTokenRecord.setFieldValue('name', cardDetails.response.customers[0].credit_card.masked_number.slice(-4));
-            
+                        motusTokenRecord.setFieldValue('custrecord_motus_cc_token_hpf', tokenData.enc_key);
+                        motusTokenRecord.setFieldValue('custrecord_motus_cc_token_enc', tokenData.enc_key);
+
                         newRec = nlapiSubmitRecord(motusTokenRecord);
                         nlapiLogExecution('ERROR', 'newRec id', newRec);
                         return newRec;
@@ -306,7 +384,7 @@
                             nlapiLogExecution('DEBUG', 'oauthToken', JSON.stringify(oauthTokenData.getBody()));
                             
                             
-                            this.submitBillingAddress(cardData)
+                            //this.submitBillingAddress(credentials_object,JSON.parse(oauthTokenData.getBody()), cardData)
                             
                             
                             var authReponse = this.getAuthResponse(credentials_object,JSON.parse(oauthTokenData.getBody()),cardData);
@@ -521,7 +599,7 @@
                                 status: 'Success'
                             }
                         }
-                    }
+                    },
                 });
                 
             });
