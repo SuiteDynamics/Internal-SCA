c65c5d72fcb517d607fc5726b1a9d3e153cb0d11
alexangulo25 <ing.alex.angulo@gmail.com>
2024-04-08 20:37:08 -0500

    Subscription development

--- Extensions/Workspace/ZoneSubscriptions/Modules/ZoneSubscriptions/JavaScript/SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions.js
+++ Extensions/Workspace/ZoneSubscriptions/Modules/ZoneSubscriptions/JavaScript/SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions.js
@@ -1,67 +1,65 @@
 
 define(
 	'SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions'
-,   [
+	, [
 		'SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions.List.View'
 	]
-,   function (
+	, function (
 		ZoneSubscriptionsListView
-	)
-{
-	'use strict';
+	) {
+		'use strict';
 
-	return  {
-		mountToApp: function mountToApp (container)
-		{
-			// using the 'Layout' component we add a new child view inside the 'Header' existing view 
-			// (there will be a DOM element with the HTML attribute data-view="Header.Logo")
-			// more documentation of the Extensibility API in
-			// https://system.netsuite.com/help/helpcenter/en_US/APIs/SuiteCommerce/Extensibility/Frontend/index.html
-			
-			/** @type {LayoutComponent} */
+		return {
+			mountToApp: function mountToApp(container) {
+				// using the 'Layout' component we add a new child view inside the 'Header' existing view 
+				// (there will be a DOM element with the HTML attribute data-view="Header.Logo")
+				// more documentation of the Extensibility API in
+				// https://system.netsuite.com/help/helpcenter/en_US/APIs/SuiteCommerce/Extensibility/Frontend/index.html
 
-			console.log('zone')
-			var pageType = container.getComponent('PageType');
-			var environment = container.getComponent("Environment");
-			var MyAccountMenu = container.getComponent("MyAccountMenu");
+				/** @type {LayoutComponent} */
 
-			MyAccountMenu.addGroup({
-				"id": "zone_advanced_billing",
-				"name": "Subscriptions",
-				"url": ("subscriptions"),
-				"index": 6
-			});
-			
-			
-			pageType.registerPageType({
-				name: 'SearchResultsModuleView',
-				routes: ['subscriptions'],
-				options: {
-					container: container,
-					environment: environment,
-				},
-				view: ZoneSubscriptionsListView
-			});
+				console.log('zone')
+				var pageType = container.getComponent('PageType');
+				var environment = container.getComponent("Environment");
+				var MyAccountMenu = container.getComponent("MyAccountMenu");
 
-			pageType.registerPageType({
-				name: 'SubscriptionDetailView',
-				routes: ['subscription-detail'],
-				view: ZoneSubscriptionsListView,
-				options: {
-					application: container,
-					environment: environment,
-				}
-			});
+				MyAccountMenu.addGroup({
+					"id": "zone_advanced_billing",
+					"name": "Subscriptions",
+					"url": ("subscriptions"),
+					"index": 6
+				});
 
-			var layout = container.getComponent('Layout');
-			
-			// if(layout)
-			// {
-			// 	layout.addChildView('Header.Logo', function() { 
-			// 		return new ZoneSubscriptionsView({ container: container });
-			// 	});
-			// }
 
-		}
-	};
-});
+				pageType.registerPageType({
+					name: 'SearchResultsModuleView',
+					routes: ['subscriptions'],
+					options: {
+						container: container,
+						environment: environment,
+					},
+					view: ZoneSubscriptionsListView
+				});
+
+				pageType.registerPageType({
+					name: 'SubscriptionDetailView',
+					routes: ['subscription-detail'],
+					view: ZoneSubscriptionsListView,
+					options: {
+						application: container,
+						environment: environment,
+					}
+				});
+
+				var layout = container.getComponent('Layout');
+
+				// if(layout)
+				// {
+				// 	layout.addChildView('Header.Logo', function() { 
+				// 		return new ZoneSubscriptionsView({ container: container });
+				// 	});
+				// }
+
+			}
+		};
+	});
--- Extensions/Workspace/ZoneSubscriptions/Modules/ZoneSubscriptions/JavaScript/ZoneSubscriptions.List.View.js
+++ Extensions/Workspace/ZoneSubscriptions/Modules/ZoneSubscriptions/JavaScript/ZoneSubscriptions.List.View.js
@@ -1,239 +1,238 @@
 // @module SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions
 define('SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions.List.View'
-,	[
-	'suitedynamics_zonesubscriptions_zonesubscriptions_list.tpl'
-	
-	,	'SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions.SS2Model'
-	
-	,	'Backbone'
-	,	'Utils'
-    ]
-, function (
-	suitedynamics_zonesubscriptions_zonesubscriptions_list_tpl
-	
-	,	ZoneSubscriptionsSS2Model
-	
-	,	Backbone
-	,	Utils
-)
-{
-    'use strict';
+	, [
+		'suitedynamics_zonesubscriptions_zonesubscriptions_list.tpl'
 
-	// @class SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions.View @extends Backbone.View
-	return Backbone.View.extend({
+		, 'SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions.SS2Model'
 
-		template: suitedynamics_zonesubscriptions_zonesubscriptions_list_tpl
+		, 'Backbone'
+		, 'Utils'
+	]
+	, function (
+		suitedynamics_zonesubscriptions_zonesubscriptions_list_tpl
 
-	,	initialize: function (options) {
+		, ZoneSubscriptionsSS2Model
 
-			/*  Uncomment to test backend communication with an example service
-				(you'll need to deploy and activate the extension first)
-			*/
-			if(!this.subscriptions){
-				this.subscriptions = [];
-			}
+		, Backbone
+		, Utils
+	) {
+		'use strict';
 
-			this.model = new ZoneSubscriptionsSS2Model();
-			var self = this;
-         	this.model.fetch().done(function(result) {
-				console.log('ZAB Subscriptions', result)
-				self.subscriptions = result;
-				self.render();
-      		});
+		// @class SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions.View @extends Backbone.View
+		return Backbone.View.extend({
 
+			template: suitedynamics_zonesubscriptions_zonesubscriptions_list_tpl
 
-			console.log('list view')
-			this.on('afterViewRender', function() {
-	
-				$("#zab-item").find("p").hide();
-				$("#zab-item").find("div").hide();
-				
-			});
-				  
-			this.savedSearchId = (options.routerArguments && options.routerArguments[0]);
-			var urlOptions = Utils.parseUrlOptions(options.routerArguments && options.routerArguments[1]);
-			var page = urlOptions['page'] || '1';
-			var sortCol = urlOptions['sortCol'];
-			var order = urlOptions['order'];
-			var groupValue = urlOptions['groupValue'];
-			var groupIndex = urlOptions['groupIndex'];
-			
-			var config = _.findWhere(options.savedSearches,{'savedSearchId': this.savedSearchId});
-			this.tabLabel = (config && config.tabLabel);
-			this.drillDownField = (config && config.drillDownField);
-			this.urlField = (config && config.urlField);
-			this.recordsPerPage = (config && config.recordsPerPage);
-			
-			this.formattedResults = [];
-			
-			this.loading = true;
-			
-			var self = this;
-			var reqObj = {
-				'savedSearchId': this.savedSearchId,
-				'pageSize': this.recordsPerPage,
-				'page': page
-			};
-			if(sortCol){
-				reqObj.sortCol = sortCol;
-				reqObj.order = order;
-			}
-			if(groupValue && groupIndex){
-				reqObj.groupValue = groupValue;
-				reqObj.groupIndex = groupIndex;
-				this.showReturn = true;
-			}
-				  
+			, initialize: function (options) {
 
+				/*  Uncomment to test backend communication with an example service
+					(you'll need to deploy and activate the extension first)
+				*/
+				if (!this.subscriptions) {
+					this.subscriptions = [];
+				}
 
-		}
-
-		,	events: {
-			'click [data-sort]': 'sortData',
-			'click [data-group]': 'drillDownView',
-			'click .return-summary-link': 'returnSummary',
-			'click [data-action="expand-table"]': 'expandTable',
-		}
-	,	returnSummary: function(){
-			var UrlHelper = _.has(UrlHelperModule,'UrlHelper')? UrlHelperModule.UrlHelper : UrlHelperModule;
-			
-			var url = Backbone.history.fragment;
-			
-			url = UrlHelper.removeUrlParameter(url, 'groupValue');
-			url = UrlHelper.removeUrlParameter(url, 'groupIndex');
-			url = UrlHelper.removeUrlParameter(url, 'page');
-			url = UrlHelper.removeUrlParameter(url, 'sortCol');
-			url = UrlHelper.removeUrlParameter(url, 'order');
-			
-			Backbone.history.navigate(url, { trigger: true });
-		}
-	,	drillDownView: function(e){
-			var value = this.$(e.currentTarget).attr('data-group');
-			var colindex = this.$(e.currentTarget).attr('data-colindex');
-			
-			var UrlHelper = _.has(UrlHelperModule,'UrlHelper')? UrlHelperModule.UrlHelper : UrlHelperModule;
-			
-			
-			var url = Backbone.history.fragment;
-			
-			url = UrlHelper.setUrlParameter(url, 'groupValue', value);
-			url = UrlHelper.setUrlParameter(url, 'groupIndex', colindex);
-			url = UrlHelper.removeUrlParameter(url, 'page');
-			url = UrlHelper.removeUrlParameter(url, 'sortCol');
-			url = UrlHelper.removeUrlParameter(url, 'order');
-			
-			Backbone.history.navigate(url, { trigger: true });
-		}
-	// , 	childViews: {
-	// 		'Pagination.View': function() {
-				
-	// 			var defaultPaginationSettings = this.options.environment.getConfig('defaultPaginationSettings',{});
-				
-	// 			var PaginationView = _.has(GlobalViewsPaginationModule,'GlobalViewsPaginationView')? GlobalViewsPaginationModule.GlobalViewsPaginationView: GlobalViewsPaginationModule;
-				
-	// 			return new PaginationView(
-	// 				_.extend(
-	// 					{
-	// 						totalPages: Math.ceil(
-	// 							this.totalRecords / this.recordsPerPage
-	// 						)
-	// 					},
-	// 					defaultPaginationSettings
-	// 				)
-	// 			);
-	// 		}
-	// 	}
-	,	getSelectedMenu: function() {
-			return 'search_' + this.savedSearchId;
-		}
-	,	sortData: function(e){
-			var UrlHelper = _.has(UrlHelperModule,'UrlHelper')? UrlHelperModule.UrlHelper : UrlHelperModule;
-			
-			var columnIndex = this.$(e.currentTarget).attr('data-index');
-			var sortdir = this.$(e.currentTarget).attr('data-sortdir');
-			
-			var sortOrder = (sortdir == "DESC")? '0': '1';
-			
-			var url = Backbone.history.fragment;
-			
-			url = UrlHelper.setUrlParameter(url, 'sortCol', columnIndex);
-			url = UrlHelper.setUrlParameter(url, 'order', sortOrder);
-			url = UrlHelper.removeUrlParameter(url, 'page');
-			
-			Backbone.history.navigate(url, { trigger: true });
-			
-		}
-	,	formatResults: function(){
-		
-			var results = this.model.get('results',[]);
-			
-			this.totalRecords = this.model.get('totalRecords') || 0;
-			
-			var self = this;
-			var formattedResults = [];
-			
-			_.each(results,function(row){
-				
-				var rowValue = []
-				_.each(row,function(result,index){
-				
-					var column = result.column;
-					var fieldText = result.value;
-					var fieldInternalid = result.value;
-					
-					if(result.text){
-						fieldText = result.text;
-						fieldInternalid = result.value;
-					}
-					
-					rowValue.push({
-						text: fieldText,
-						internalid: fieldInternalid,
-						colindex: index,
-						isGroup: ((column.name == self.drillDownField) && !self.showReturn),
-						isURL: (column.name == self.urlField)
-					});
-					
+				this.model = new ZoneSubscriptionsSS2Model();
+				var self = this;
+				this.model.fetch().done(function (result) {
+					console.log('ZAB Subscriptions', result)
+					self.subscriptions = result;
+					self.render();
 				});
-				formattedResults.push(rowValue);
-			});
-		
-			
-			this.formattedResults = formattedResults;
-		}
-	,	expandTable: function(event){
-		
-		console.log(event.target)
-		
-		let targetID = $(event.target)[0].id;
 
-		console.log(targetID)
-		if(targetID == "zab-detail-button"){
-			return
-		}
-		event.stopPropagation();
-		var $target = $(event.target);
-		if ( $target.closest("td").attr("colspan") > 1 ) {
-			$target.slideUp();
-		} else {
-			console.log($target.closest("tr").next().find("p"))
-			console.log($target.closest("tr").next().find("div"))
-			$target.closest("tr").next().find("p").slideToggle();
-			$target.closest("tr").next().find("div").slideToggle();
-		}                    
-	}
 
-		//@method getContext @return SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions.View.Context
-	,	getContext: function getContext()
-		{
+				console.log('list view')
+				this.on('afterViewRender', function () {
 
-			console.log('getcontext', this)
-			//@class SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions.View.Context
-			// this.message = this.message || 'Hello World!!'
-			return {
-				hasResults: (this.subscriptions.length > 0),
-				subscriptions: this.subscriptions
-			};
-		}
+					$("#zab-item").find("p").hide();
+					$("#zab-item").find("div").hide();
+
+				});
+
+				this.savedSearchId = (options.routerArguments && options.routerArguments[0]);
+				var urlOptions = Utils.parseUrlOptions(options.routerArguments && options.routerArguments[1]);
+				var page = urlOptions['page'] || '1';
+				var sortCol = urlOptions['sortCol'];
+				var order = urlOptions['order'];
+				var groupValue = urlOptions['groupValue'];
+				var groupIndex = urlOptions['groupIndex'];
+
+				var config = _.findWhere(options.savedSearches, { 'savedSearchId': this.savedSearchId });
+				this.tabLabel = (config && config.tabLabel);
+				this.drillDownField = (config && config.drillDownField);
+				this.urlField = (config && config.urlField);
+				this.recordsPerPage = (config && config.recordsPerPage);
+
+				this.formattedResults = [];
+
+				this.loading = true;
+
+				var self = this;
+				var reqObj = {
+					'savedSearchId': this.savedSearchId,
+					'pageSize': this.recordsPerPage,
+					'page': page
+				};
+				if (sortCol) {
+					reqObj.sortCol = sortCol;
+					reqObj.order = order;
+				}
+				if (groupValue && groupIndex) {
+					reqObj.groupValue = groupValue;
+					reqObj.groupIndex = groupIndex;
+					this.showReturn = true;
+				}
+
+
+
+			}
+
+			, events: {
+				'click [data-sort]': 'sortData',
+				'click [data-group]': 'drillDownView',
+				'click .return-summary-link': 'returnSummary',
+				'click [data-action="expand-table"]': 'expandTable',
+			}
+			, returnSummary: function () {
+				var UrlHelper = _.has(UrlHelperModule, 'UrlHelper') ? UrlHelperModule.UrlHelper : UrlHelperModule;
+
+				var url = Backbone.history.fragment;
+
+				url = UrlHelper.removeUrlParameter(url, 'groupValue');
+				url = UrlHelper.removeUrlParameter(url, 'groupIndex');
+				url = UrlHelper.removeUrlParameter(url, 'page');
+				url = UrlHelper.removeUrlParameter(url, 'sortCol');
+				url = UrlHelper.removeUrlParameter(url, 'order');
+
+				Backbone.history.navigate(url, { trigger: true });
+			}
+			, drillDownView: function (e) {
+				var value = this.$(e.currentTarget).attr('data-group');
+				var colindex = this.$(e.currentTarget).attr('data-colindex');
+
+				var UrlHelper = _.has(UrlHelperModule, 'UrlHelper') ? UrlHelperModule.UrlHelper : UrlHelperModule;
+
+
+				var url = Backbone.history.fragment;
+
+				url = UrlHelper.setUrlParameter(url, 'groupValue', value);
+				url = UrlHelper.setUrlParameter(url, 'groupIndex', colindex);
+				url = UrlHelper.removeUrlParameter(url, 'page');
+				url = UrlHelper.removeUrlParameter(url, 'sortCol');
+				url = UrlHelper.removeUrlParameter(url, 'order');
+
+				Backbone.history.navigate(url, { trigger: true });
+			}
+			// , 	childViews: {
+			// 		'Pagination.View': function() {
+
+			// 			var defaultPaginationSettings = this.options.environment.getConfig('defaultPaginationSettings',{});
+
+			// 			var PaginationView = _.has(GlobalViewsPaginationModule,'GlobalViewsPaginationView')? GlobalViewsPaginationModule.GlobalViewsPaginationView: GlobalViewsPaginationModule;
+
+			// 			return new PaginationView(
+			// 				_.extend(
+			// 					{
+			// 						totalPages: Math.ceil(
+			// 							this.totalRecords / this.recordsPerPage
+			// 						)
+			// 					},
+			// 					defaultPaginationSettings
+			// 				)
+			// 			);
+			// 		}
+			// 	}
+			, getSelectedMenu: function () {
+				return 'search_' + this.savedSearchId;
+			}
+			, sortData: function (e) {
+				console.log('Entering here')
+				var UrlHelper = _.has(UrlHelperModule, 'UrlHelper') ? UrlHelperModule.UrlHelper : UrlHelperModule;
+
+				var columnIndex = this.$(e.currentTarget).attr('data-index');
+
+				var sortdir = this.$(e.currentTarget).attr('data-sortdir');
+				console.log(sortdir)
+				var sortOrder = (sortdir == "DESC") ? '0' : '1';
+
+				var url = Backbone.history.fragment;
+
+				url = UrlHelper.setUrlParameter(url, 'sortCol', columnIndex);
+				url = UrlHelper.setUrlParameter(url, 'order', sortOrder);
+				url = UrlHelper.removeUrlParameter(url, 'page');
+
+				Backbone.history.navigate(url, { trigger: true });
+
+			}
+			, formatResults: function () {
+
+				var results = this.model.get('results', []);
+
+				this.totalRecords = this.model.get('totalRecords') || 0;
+
+				var self = this;
+				var formattedResults = [];
+
+				_.each(results, function (row) {
+
+					var rowValue = []
+					_.each(row, function (result, index) {
+
+						var column = result.column;
+						var fieldText = result.value;
+						var fieldInternalid = result.value;
+
+						if (result.text) {
+							fieldText = result.text;
+							fieldInternalid = result.value;
+						}
+
+						rowValue.push({
+							text: fieldText,
+							internalid: fieldInternalid,
+							colindex: index,
+							isGroup: ((column.name == self.drillDownField) && !self.showReturn),
+							isURL: (column.name == self.urlField)
+						});
+
+					});
+					formattedResults.push(rowValue);
+				});
+
+
+				this.formattedResults = formattedResults;
+			}
+			, expandTable: function (event) {
+
+				console.log(event.target)
+
+				let targetID = $(event.target)[0].id;
+
+				console.log(targetID)
+				if (targetID == "zab-detail-button") {
+					return
+				}
+				event.stopPropagation();
+				var $target = $(event.target);
+				if ($target.closest("td").attr("colspan") >= 0) {
+					$target.slideUp();
+				} else {
+					var $nextRow = $target.closest("tr").next("tr#zab-item");
+					$nextRow.slideToggle();
+
+				}
+			}
+
+			//@method getContext @return SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions.View.Context
+			, getContext: function getContext() {
+
+				console.log('getcontext', this)
+				//@class SuiteDynamics.ZoneSubscriptions.ZoneSubscriptions.View.Context
+				// this.message = this.message || 'Hello World!!'
+				return {
+					hasResults: (this.subscriptions.length > 0),
+					subscriptions: this.subscriptions
+				};
+			}
+		});
 	});
-});
